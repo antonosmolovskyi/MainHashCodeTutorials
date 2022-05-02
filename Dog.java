5d6a5f3594c36ab4bfdfaa7eb98d27667afa3d5b
Anton <tony.osmolovsky@gmail.com>
2022-05-02 18:04:35 +0200

    Creat and push class named Dog

---
+++ Dog.java
@@ -0,0 +1,24 @@
+package classwork19;
+
+public class Dog {
+	private String name;
+
+	public Dog(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Dog [name=" + name + "]";
+	}
+	
+}
